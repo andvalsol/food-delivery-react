922fddf21b46f42503566e4343fbee99fa14f055
Carlos Valverde Solera <and_val9@hotmail.com>
2021-02-26 11:21:06 -0600

    HomeScreen.js finished

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -40,40 +40,40 @@ const HomeScreen = (props) => {
         setSelectedCategory(category)
     }
 
-    const renderItem = (item) => {
-        return (
-            <TouchableOpacity
-                style={
-                    [
-                    ...styles.singleHorizontalCategory,
-                    {backgroundColor: (selectedCategory?.id === item.id) ? COLORS.primary : COLORS.white}
-                    ]
-                }
-                onPress={() => onSelectCategory(item)}>
-                <View style={
-                    [
-                        ...styles.singleHorizontalBackground,
-                        {backgroundColor: (selectedCategory?.id === item.id) ? COLORS.white : COLORS.lightGray}
-                    ]
-                }>
-                    <Image
-                        style={styles.singleHorizontalImage}
-                        source={item.icon}
-                        resizeMode="contain"/>
-                </View>
-                <Text style={
-                    [
-                        ...styles.singleHorizontalText,
-                        {backgroundColor: (selectedCategory?.id === item.id) ? COLORS.white : COLORS.black}
-                    ]
-                }>
-                    {item.name}
-                </Text>
-            </TouchableOpacity>
-        )
-    }
-
     const renderMainCategories = () => {
+        const renderItem = (item) => {
+            return (
+                <TouchableOpacity
+                    style={
+                        [
+                            ...styles.singleHorizontalCategory,
+                            {backgroundColor: (selectedCategory?.id === item.id) ? COLORS.primary : COLORS.white}
+                        ]
+                    }
+                    onPress={() => onSelectCategory(item)}>
+                    <View style={
+                        [
+                            ...styles.singleHorizontalBackground,
+                            {backgroundColor: (selectedCategory?.id === item.id) ? COLORS.white : COLORS.lightGray}
+                        ]
+                    }>
+                        <Image
+                            style={styles.singleHorizontalImage}
+                            source={item.icon}
+                            resizeMode="contain"/>
+                    </View>
+                    <Text style={
+                        [
+                            ...styles.singleHorizontalText,
+                            {backgroundColor: (selectedCategory?.id === item.id) ? COLORS.white : COLORS.black}
+                        ]
+                    }>
+                        {item.name}
+                    </Text>
+                </TouchableOpacity>
+            )
+        }
+
         return (
             <View style={styles.categoryHeader}>
                 <Text style={{...FONTS.h1}}>
@@ -93,10 +93,90 @@ const HomeScreen = (props) => {
         )
     }
 
+    const getCategoryNameById = (categoryId) => {
+        const category = categories.filter((category) => category.id === categoryId)
+
+        if (category.length > 0)
+            return category[0].name
+
+        return ""
+    }
+
+    const renderRestaurantList = () => {
+        const singleCategory = (categoryId) => (
+            <View
+                style={styles.singleRestaurantMealCategory}
+                key={categoryId}>
+                <Text
+                    style={styles.singleRestaurantCategoryText}>
+                    {getCategoryNameById(categoryId)}
+                </Text>
+            </View>
+        )
+
+        const priceRating = (priceRating, itemPriceRating) => (
+            <Text
+                style={
+                    [
+                        ...styles.singleRestaurantPriceRatingText,
+                        {
+                            color: (priceRating <= itemPriceRating) ? COLORS.BLACK : COLORS.darkgray
+                        }
+                    ]
+                }
+                key={priceRating}>
+
+            </Text>
+        )
+
+        const renderItem = (item) => {
+            return (
+                <TouchableOpacity
+                    style={styles.singleRestaurantContainer}>
+                    <View style={{marginBottom: SIZES.padding}}>
+                        <Image
+                            source={item.photo}
+                            resizeMode="cover"
+                            style={styles.singleRestaurantImage}/>
+                    </View>
+                    <View style={styles.singleRestaurantSubheaderBackground}>
+                        <Text style={styles.singleRestaurantSubheaderText}>{item.duration}</Text>
+                    </View>
+                    <Text style={styles.singleRestaurantName}>{item.name}</Text>
+                    <View style={styles.singleRestaurantInfoSection}>
+                        <Image
+                            style={styles.singleRestaurantRatingImage}
+                            source={icons.star}/>
+                            <Text
+                                style={styles.singleRestaurantRatingText}>
+                                {item.rating}
+                            </Text>
+                        <View
+                            style={styles.singleRestaurantMealContainer}>
+                            {item.categories.map((category) => singleCategory(category))}
+                            {
+                                [1, 2, 3].map((priceRating) => priceRating(priceRating, item.priceRating))
+                            }
+                        </View>
+                    </View>
+                </TouchableOpacity>
+            )
+        }
+
+        return (
+            <FlatList
+                data={restaurantData}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
+                contentContainerStyle={styles.restaurantListContainer}/>
+        )
+    }
+
     return ( // SafeAreaView aligns items from top to bottom
         <SafeAreaView style={styles.container}>
             {renderHeader()}
             {renderMainCategories()}
+            {renderRestaurantList()}
         </SafeAreaView>
     )
 }
@@ -172,6 +252,62 @@ const styles = StyleSheet.create({
     singleHorizontalText: {
         marginTop: SIZES.padding,
         ...FONTS.body5
+    },
+    restaurantListContainer: {
+        paddingHorizontal: SIZES.padding * 2,
+        paddingBottom: 30 // This will avoid the last item to be blocked by the bottom navigation bar
+    },
+    singleRestaurantContainer: {
+        marginBottom: SIZES.padding * 2
+    },
+    singleRestaurantImage: {
+        width: "100%",
+        height: 200,
+        borderRadius: SIZES.radius
+    },
+    singleRestaurantSubheaderBackground: {
+        position: "absolute",
+        bottom: 0,
+        height: 50,
+        width: SIZES.width * 0.3,
+        backgroundColor: COLORS.white,
+        borderTopRightRadius: SIZES.radius,
+        borderBottomLeftRadius: SIZES.radius,
+        alignItems: "center",
+        justifyContent: "center",
+        ...styles.shadow
+    },
+    singleRestaurantSubheaderText: {
+        ...FONTS.h4
+    },
+    singleRestaurantName: {
+        ...FONTS.body2
+    },
+    singleRestaurantInfoSection: {
+        marginTop: SIZES.padding * 2,
+        flexDirection: "row"
+    },
+    singleRestaurantRatingImage: {
+        width: 20,
+        height: 20,
+        tintColor: COLORS.primary,
+        marginRight: 10
+    },
+    singleRestaurantRatingText: {
+        ...FONTS.body3
+    },
+    singleRestaurantMealContainer: {
+        flexDirection: "row",
+        marginLeft: 10
+    },
+    singleRestaurantMealCategory: {
+        flexDirection: "row"
+    },
+    singleRestaurantCategoryText: {
+        ...FONTS.body3
+    },
+    singleRestaurantPriceRatingText: {
+        ...FONTS.body3
     }
 })
 
